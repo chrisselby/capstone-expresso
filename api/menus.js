8a9fc949a0ac451da5700329d75688fdb9c8bea4
Christopher Selby <cselby@Christophers-MacBook-Pro.local>
2018-01-15 20:09:04 -0800

    Init git, so far added routes for employees, timesheets, and a get for menus with tests passing

---
+++ api/menus.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const sqlite3 = require('sqlite3');
+
+const menusRouter = express.Router();
+
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+
+menusRouter.get('/', (req, res, next) => {
+    db.all("SELECT * FROM Menu", (err, menus) => {
+        if (err) {
+            next(err);
+        } else {
+            res.status(200).json({menus: menus});
+        }
+    })
+});
+
+module.exports = menusRouter;
